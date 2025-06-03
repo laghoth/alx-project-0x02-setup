75af4526a411574c54b1bfcc4aaee16448020595
OTHMANE LAGHLIMI <laghlim.othmane@gmail.com>
2025-06-03 12:18:48 +0100

    commit 14

--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 const AboutPage: React.FC = () => {
   return (
     <>
       <Header />
-      <main className="p-6">
+      <main className="p-6 space-y-4">
         <h1 className="text-2xl font-bold">About Page</h1>
-        <p>This page tells you more about the ALX Project setup.</p>
+        <div className="flex gap-4 flex-wrap">
+          <Button label="Small Rounded-sm" size="small" shape="rounded-sm" />
+          <Button label="Medium Rounded-md" size="medium" shape="rounded-md" />
+          <Button
+            label="Large Rounded-full"
+            size="large"
+            shape="rounded-full"
+          />
+        </div>
       </main>
     </>
   );
